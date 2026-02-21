2fa3d4c1640cdc6ad481ae6324589d90167643f9
Natthavee9 <y.natthavee@gmail.com>
2026-02-21 23:35:45 +0700

    edit navbar

--- frontend/src/component/navbar.tsx
+++ frontend/src/component/navbar.tsx
@@ -1,14 +1,20 @@
-// src/components/Navbar.tsx
+import logo from "../assets/unnamed-removebg-preview.png";
+
 export default function Navbar() {
   return (
-    <div className="navbar bg-base-100 shadow-bg">
+    <div className="w-full px-8 py-4 min-h-16 flex items-center justify-between shadow-sm">
       <div className="navbar-start">
-        <a className="btn btn-ghost text-xl">Healthy Sense</a>
+        <a className="flex items-center gap-2 text-3xl font-bold bg-gradient-to-r from-green-500 to-cyan-500 bg-clip-text text-transparent">
+          <img src={logo} alt="Healthy Sense Logo" className="h-10 w-auto" />
+          Healthy Sense
+        </a>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li><a href="/">Home</a></li>
-          <li><a href="/dashboard">Dashboard</a></li>
+        <ul className="menu menu-horizontal px-2 text-xl text-green-700">
+          <li><a href="/" className="hover:text-cyan-500 hover:!bg-transparent active:!bg-transparent focus:!bg-transparent transition-colors">Machine Learning</a></li>
+          <li><a href="/" className="hover:text-cyan-500 hover:!bg-transparent active:!bg-transparent focus:!bg-transparent transition-colors">Neural Network</a></li>
+          <li><a href="/" className="hover:text-cyan-500 hover:!bg-transparent active:!bg-transparent focus:!bg-transparent transition-colors">Demo ML</a></li>
+          <li><a href="/" className="hover:text-cyan-500 hover:!bg-transparent active:!bg-transparent focus:!bg-transparent transition-colors">Demo NN</a></li>
         </ul>
       </div>
     </div>
